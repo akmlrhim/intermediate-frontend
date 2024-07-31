eeba83f1103b3ff03fd7f34564e23201a0eef084
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2024-07-31 15:17:41 +0800

    Detail #2

--- lit/src/js/components/title-app.js
+++ lit/src/js/components/title-app.js
@@ -2,8 +2,15 @@ import { LitElement, html, css } from 'lit';
 
 class TitleApp extends LitElement {
   static styles = css`
+    :host {
+      display: block;
     h1 {
       text-align: center;
+    }
+
+    h1 {
+      font-size: 24px;
+      margin-top: 0;
     }
   `;
 
